0275dc4fc34cb3a6575836ade85f161b8431f097
IbrahimElbasheer <Ibrahim_Elbasheer@brown.edu>
2025-11-03 04:36:05 -0500

    Loading Data and Installing Packages

---
+++ BHDS-2010-Assignment-3.R
@@ -0,0 +1,16 @@
+#setwd("D/Colleges/Brown University/BHDS 2010/Module 8/Assignment 3/BHDS-2010-Assignment-3")
+TextMessages<-read.csv("TextMessages.csv")
+head(TextMessages)
+
+#First, we install the reshape package 
+
+install.packages("reshape2")
+
+#Load the package
+
+library(reshape2)
+library(ggplot2)
+#===============================================================================
+
+
+
