5c5ada57e2b8a394d9644828ebf2d399ccd69561
IbrahimElbasheer <Ibrahim_Elbasheer@brown.edu>
2025-11-03 13:31:16 -0500

    Adjusting the data and plots That is to prepare for easy mirage

--- BHDS-2010-Assignment-3.R
+++ BHDS-2010-Assignment-3.R
@@ -14,14 +14,17 @@ library(ggplot2)
 
 #Converting Data to Long Format:
 
-TextMessages_long<- melt(TextMessages, id = c("Participant","Group"),
-                         measured =c(Baseline,Six_months))
-
+TextMessages_long <- TextMessages %>%
+  pivot_longer(
+    cols = c(Baseline, Six_months),
+    names_to = "Time",
+    values_to = "Messages"
+  )
 #===============================================================================
 
 
-barplot_d_by_Group_and_Time<-ggplot(TextMessages_long,aes(x = variable,
-                                                  y = 1:100, fill = Group))
+barplot_d_by_Group_and_Time<-ggplot(TextMessages_long,aes(x = Time,
+                                                  y = Messages, fill = Group))
 barplot_d_by_Group_and_Time+ 
   stat_summary(fun.data = mean_cl_normal,geom = "bar", colour = "blue" ,
                fill="lightblue") +
@@ -29,4 +32,5 @@ barplot_d_by_Group_and_Time+
        x="Average Number of Text Messages",
        y="Time") +
   facet_wrap(.~Group, nrow = 1)
+
 
